c455efed3451bee9fc1ed762aa26617f53235148
Helen <hu.heleny@gmail.com>
2017-10-11 15:21:42 -0400

    Complete chapter 2 exercises

--- chapter_two.js
+++ chapter_two.js
@@ -1,1 +1,37 @@
-console.log("hello");
+// Looping a triangle
+var hash = '#'
+for (var i = 0; i < 7; i++) {
+	console.log(hash)
+	hash += '#';
+}
+
+// FizzBuzz
+for (var i = 1; i <= 100; i++) {
+	var res = ''
+	if (i%3 === 0) {
+		res += "Fizz";
+	}
+	if (i%5 === 0) {
+		res += "Buzz";
+	}
+	if (res === '')
+		console.log(i);
+	else
+		console.log(res);
+}
+
+// Chess Board
+var size = 8;
+var res = "";
+for (var i=0; i<size; i++) {
+	for (var j=0; j<size; j++) {
+		if ((i+j)%2==0) {
+			res += " ";
+		}
+		else {
+			res += "#";
+		}
+	}
+	res += "\n";
+}
+console.log(res);
